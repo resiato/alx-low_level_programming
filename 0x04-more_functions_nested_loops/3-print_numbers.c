7215db8e15b85e8d4b526e06e90f87bb462bd695
resiato <resiatolaitete@gmail.com>
2021-06-24 10:41:58 -0700

    more functions practices

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for(a = 0; a <= 9; a++)
+	{
+		_putchar(a + '0');
+	}
+
+	_putchar('\n');
+}
